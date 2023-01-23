8015b205533396574d5166f065afd245914b2cae
Komla Adzam <adzamkomla.dev@gmail.com>
2023-01-23 15:44:22 +0000

    Add app layout

---
+++ src/TransPoster.MVC/ViewComponents/Shared/NavViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransPoster.MVC.ViewComponents.Shared;
+
+public class NavViewComponent : ViewComponent
+{
+    private readonly ILogger<NavViewComponent> _logger;
+
+    public NavViewComponent(ILogger<NavViewComponent> logger) => _logger = logger;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        List<string> items = new()
+        {
+            "Hello",
+            "Hi",
+            "Face"
+        };
+
+        _logger.LogInformation("These are the items: ", items);
+
+        return View(items);
+    }
+}
