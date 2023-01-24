b1cf6caba6b94fd870e6e217abc08afe7845ce0f
Komla Adzam <adzamkomla.dev@gmail.com>
2023-01-24 09:00:05 +0000

    Add auth layout

---
+++ src/TransPoster.MVC/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransPoster.MVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
